507aaebd8d08344f2351ff4e5b4f86eaa76ad3a3
kongyunhui <1032316751@qq.com>
2017-02-24 09:52:02 +0800

    base

---
+++ src/main/java/com/kyh/base/User.java
@@ -0,0 +1,41 @@
+package com.kyh.base;
+
+/**
+ * Created by kongyunhui on 2017/2/23.
+ */
+public class User {
+    private int id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
